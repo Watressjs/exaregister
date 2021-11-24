83e8223530703192ef1349a256fc2f889b470e1b
Lewis <81768916+lewis1901@users.noreply.github.com>
2021-11-24 19:45:03 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,64 +1,40 @@
 module.exports = {
     bot: {
-        botOwner: ["531848956985081856"],
-        botToken: "OTEyMDQxNDQ3NTY4NjA1Mjk0.YZqKyg.mecza6kizImODhWHy7MmctOevis",
+        botOwner: ["851025562607681536"],
+        botToken: "",
         botPrefix: ".",
-        botStatus: "TheÈXÀ#1562 | Watress Was Here.",
-        mongoURL: "mongodb+srv://lewisjs:okan1234@cluster0.o8u51.mongodb.net/kayıtbot?retryWrites=true&w=majority",
+        botStatus: "Joseph Assistant",
+        mongoURL: "mongodb+srv://lewisjs:okan1234@cluster0.o8u51.mongodb.net/Guardbot?retryWrites=true&w=majority",
     },
     roles: {
-
-        registerStaff: ["910594003790069770"],
-
-        manRoles: ["910594003748159500","910594003748159499","910594003748159498"], // erkek rolleri
-
-        womanRoles: ["910594003748159506","910594003748159505","910594003748159504"], // kız rolleri
-
-        unregisterRoles: ["910594003735556122"],
-
-        tagRole: "910594003773296644",
-
-        vipRole: "910594003760734268",
-
+        registerStaff: [911606452081278976],
+        manRoles: [911603276309467136], // erkek rolleri
+        womanRoles: [911603272673005618], // kız rolleri
+        unregisterRoles: [910911500447793163],
+        tagRole: "911615725427359764",
+        vipRole: "911617418743394317",
         boosterRole: "",
-
-        suspecious: "910594003718787072", // şüpheli hesap rolü
-
+        suspecious: "911616340673036338", // şüpheli hesap rolü
     },
-
     channels: {
-
-        registerChannel: "910594004171759640",
-
-        rulesChannel: "910594004448608330",
-
-        botVoice: "911611543035191318",
+        registerChannel: "912370056430776360",
+        rulesChannel: "912371170999951360",
+        botVoice: "912371051126739025
+",
 
     },
-
     guild: {
-
-        guildID: "910594003370664027",
-        tag: "・", // BURAYA İSMİN BAŞINA GELECEK TAG BİRDEN ÇOK TAGINIZ VARSA AŞŞAĞIYA GİRİN
-
-        tagges: ["#1514"], // BİRDEN ÇOK TAGINIZ VARSA BURAYA GİRİN ÖRNEK: #0001
-
-        defaultTag: "",
-
-        defaultName: "・ İsim | Yaş",
-
-        suspeciousName: "・Şüpheli | Hesap",
-
+        guildID: "901819030187409478",
+        tag: "", // BURAYA İSMİN BAŞINA GELECEK TAG BİRDEN ÇOK TAGINIZ VARSA AŞŞAĞIYA GİRİN
+        tagges: [], // BİRDEN ÇOK TAGINIZ VARSA BURAYA GİRİN ÖRNEK: #0001
+        defaultTag: "•",
+        defaultName: "• İsim | Yaş",
+        suspeciousName: "• Şüpheli | Hesap",
     },
-
-    emojis: { 
-
-        yes: "912036897197555762",
-
-        no: "912036900594933800",
-
+    emojis: {
+        yes: "912765282391580692",
+        no: "912765320958214154",
         // EMOJİLERİN ID GİRCEKSİNİZ SADECE ID
-
     }
-    
-  };
+
+};
